854f3fb875305264c8a548aecc1f75804397299a
arison <arison@outlook.com>
2022-01-06 13:22:01 +0800

    init repository

---
+++ qy.wx.robots/WXMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace qy.wx.robots
+{
+    /// <summary>
+    /// 企业微信消息类
+    /// </summary>
+    public class WXMessage
+    {
+    }
+}
